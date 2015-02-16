8ecbf520adbbd68f9e2d87ddce27fc880a2b6b2f
Nina Baculinao <nb2406@columbia.edu>
2015-02-16 17:05:14 -0500

    feat(gesturereader.py): read all images in a directory

--- gesturereader.py
+++ gesturereader.py
@@ -25,11 +25,20 @@ if __name__ == "__main__": main()
 
 import cv2
 import numpy as np
+import os
 # import matplotlib.pyplot as plt
 
 gray_img = cv2.imread('test_image.png', cv2.IMREAD_GRAYSCALE) # default has color
 cv2.imwrite('test_image_grayscale.jpg',gray_img)
 
+imageformat=".JPG"
+path="./images"
+imfilelist=[os.path.join(path,f) for f in os.listdir(path) if f.endswith(imageformat)]
+for el in imfilelist:
+        print el
+        image = cv2.imread(el, cv2.CV_LOAD_IMAGE_COLOR)
+        cv2.imshow('Image', image) #Show the image
+        cv2.waitKey(1000)
 '''
 plt.imshow(bgr_img, cmap = plt.get_cmap('gray'))
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
