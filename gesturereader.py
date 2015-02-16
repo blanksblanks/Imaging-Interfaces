52522f1cce86a587941c2ab0c7a9eba728126ac3
Nina Baculinao <nb2406@columbia.edu>
2015-02-16 17:17:31 -0500

    fix(gesturereader.py): return error and exit if dir doesn't exist

--- gesturereader.py
+++ gesturereader.py
@@ -23,22 +23,25 @@ if __name__ == "__main__": main()
 
 '''
 
-import cv2
+import cv2, os, sys
 import numpy as np
-import os
 # import matplotlib.pyplot as plt
 
 gray_img = cv2.imread('test_image.png', cv2.IMREAD_GRAYSCALE) # default has color
 cv2.imwrite('test_image_grayscale.jpg',gray_img)
 
 imageformat=".JPG"
-path="./images"
-imfilelist=[os.path.join(path,f) for f in os.listdir(path) if f.endswith(imageformat)]
-for el in imfilelist:
+path = sys.argv[1]
+# path="./images"
+if os.path.exists(path):
+    imfilelist=[os.path.join(path,f) for f in os.listdir(path) if f.endswith(imageformat)]
+    for el in imfilelist:
         print el
-        image = cv2.imread(el, cv2.CV_LOAD_IMAGE_COLOR)
+        image = cv2.imread(el, cv2.CV_LOAD_IMAGE_GRAYSCALE)
         cv2.imshow('Image', image) #Show the image
         cv2.waitKey(1000)
+else:
+	sys.exit("The path name does not exist")
 '''
 plt.imshow(bgr_img, cmap = plt.get_cmap('gray'))
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
