b2cd39fb859c20b610520b5804fd14b2d0287e6c
Nina Baculinao <nb2406@columbia.edu>
2015-02-16 17:28:26 -0500

    feat(gesturereader.py): rotate images to correct orientation bc my phone took them sideways

--- gesturereader.py
+++ gesturereader.py
@@ -37,12 +37,17 @@ if os.path.exists(path):
     imfilelist=[os.path.join(path,f) for f in os.listdir(path) if f.endswith(imageformat)]
     for el in imfilelist:
         print el
-        image = cv2.imread(el, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        image = cv2.imread(el, cv2.IMREAD_GRAYSCALE)
         # resize image - calculate aspect ratio first
         r = 300.0 / image.shape[1]
         dim = (300, int(image.shape[0] * r))
         image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        cv2.imshow('Image', image) #Show the image
+        # rotate image by 180 degrees - calculate center first
+        (h, w) = image.shape[:2]
+        center = (w / 2, h / 2)
+        M = cv2.getRotationMatrix2D(center, 270, 1.0)
+        image = cv2.warpAffine(image, M, (w, h))
+        cv2.imshow('Image', image) # finally, show the image
         cv2.waitKey(1000)
 else:
 	sys.exit("The path name does not exist")
