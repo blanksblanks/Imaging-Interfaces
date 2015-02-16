1ace02b979523ed6f73fbb0cfb3318db72decb1e
Nina Baculinao <nb2406@columbia.edu>
2015-02-16 13:56:21 -0500

    style(cam.py): adjust input and function structure

--- cam.py
+++ cam.py
@@ -1,33 +1,14 @@
-# from __future__ import print_function
-import cv2
+import cv2, time, sys
 import numpy as np
-import time
-import sys
-
-
-
-def countdown(t):
-    while t >= 0:
-        mins, secs = divmod(t, 60)
-        timeformat ='\r{:02d}:{:02d}'.format(mins, secs)
-        # print(timeformat, end='\r')
-        # print(timeformat)
-        sys.stdout.write(timeformat)
-        sys.stdout.flush()
-        time.sleep(1)
-        t -= 1
-    print('\nTaking your picture now!\n\n\n\n\n')
-    #get_image()
-
-countdown(15)
 
 # Camera 0 is the integrated web cam
 camera_port = 0
 
 # Number of frames to throw away while the camera adjusts to light levels
 ramp_frames = 30
+timer = 15
 
-# Now we can initialize the camera capture object with the cv2.VideoCapture class.
+# Initialize the camera capture object with the cv2.VideoCapture class.
 # All it needs is the index to a camera port.
 camera = cv2.VideoCapture(camera_port)
 
@@ -37,12 +18,25 @@ def get_image():
      retval, im = camera.read()
      return im
 
-# Ramp the camera - these frames will be discarded and are only used to
-# adjust light levels, if necessary
-for i in xrange(ramp_frames):
-    temp = get_image()
 
-print("Taking image...")
+def countdown(t):
+    while t > 0:
+    # Ramp the camera - these frames will be discarded and are only used to
+    # adjust light levels, if necessary
+        for i in xrange(ramp_frames/timer):
+            temp = get_image()
+        # Print countdown
+        mins, secs = divmod(t, 60)
+        timeformat ='\r                ... {:02d}:{:02d} seconds\n'.format(mins, secs)
+        sys.stdout.write(timeformat)
+        sys.stdout.flush()
+        time.sleep(1)
+        t -= 1
+
+print('Taking image in ... 00:{:02d} seconds'.format(timer))
+countdown(timer - 1)
+print('Taking image!\n')
+
 # Take the actual image we want to keep
 camera_capture = get_image()
 file = "./test_image.png"
@@ -53,5 +47,7 @@ cv2.imwrite(file, camera_capture)
 # You'll want to release the camera, otherwise you won't be able to create a new
 # capture object until your script exits
 del(camera)
+
+
 
 
