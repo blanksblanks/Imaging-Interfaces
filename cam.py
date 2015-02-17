4af351534e02a75f7796be181126fe973ffd4493
Nina Baculinao <nb2406@columbia.edu>
2015-02-17 03:42:04 -0500

    refactor(cam.py): change camera dimensions

--- cam.py
+++ cam.py
@@ -3,15 +3,14 @@ import numpy as np
 
 # Camera 0 is the integrated web cam
 camera_port = 0
-
-# Number of frames to throw away while the camera adjusts to light levels
-ramp_frames = 30
 timer = 5
 file_count = 1
 
 # Initialize the camera capture object with the cv2.VideoCapture class.
 # All it needs is the index to a camera port.
 camera = cv2.VideoCapture(camera_port)
+camera.set(3, 640)
+camera.set(4, 480)
 
 
 # Captures a single image from the camera and returns it in PIL format
@@ -23,19 +22,17 @@ def get_image():
 def write_file_to_disk():
     global file_count
     global camera
-    # Take the actual image we want to keep
+    # take the actual image we want to keep
     print('\nTaking image!')
     camera_capture = get_image()
     file = "./" + str(int(time.time())) + "_"+ str(file_count) + ".png"
-    # A nice feature of the imwrite method is that it will automatically choose the
+    # nice feature of the imwrite method is that it will automatically choose the
     # correct format based on the file extension you provide. Convenient!
     cv2.imwrite(file, camera_capture)
     file_count += 1
 
-    # You'll want to release the camera, otherwise you won't be able to create a new
-    # capture object until your script exits
+
     if file_count > 3:
-        # del(camera)
         camera.release()
     else:
         open_camera_feed()
