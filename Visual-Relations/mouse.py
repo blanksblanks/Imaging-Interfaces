efa925d7ff59ca9b6dd1f22b8b10c5305f7285ee
Nina Baculinao <nb2406@columbia.edu>
2015-04-02 15:27:08 -0400

    refactor(mouse.py): analyze labeled map while displaying binary map, clean up print statements and drawing enabled

--- Visual-Relations/mouse.py
+++ Visual-Relations/mouse.py
@@ -32,15 +32,13 @@ img = cv2.imread('ass3-campus.pgm', cv2.IMREAD_UNCHANGED)
 img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 im = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)
 # img = np.zeros((512,512,3), np.uint8)
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
 
+# Analyze image
 buildings = {}
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 contours,hierarchy = cv2.findContours(imgray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 idx = 27
 for cnt in contours:
-    print idx
     # To get all the points which comprise an object
     # Numpy function gives coordinates in (row, col)
     # OpenCV gives coordinates in (x,y)
@@ -49,45 +47,53 @@ for cnt in contours:
     cv2.drawContours(mask,[cnt],0,255,-1)
     pixelpoints = np.transpose(np.nonzero(mask))
     #pixelpoints = cv2.findNonZero(mask)
+
+    # Use color to determine index and identity
     color = cv2.mean(im,mask = mask)
-    if (color[0] < 0.9):
+    if (color[2] < 0.9):
         print 'ignored the cs courtyard'
         continue
-    print 'building', color
-    idx -= 1
+    idx = int(color[2])
+    print 'Building', idx
+
     # Let (x,y) be top-left coordinate and (w,h) be width and height
     # Find min, max value of x, min, max value of y
     x,y,w,h = cv2.boundingRect(cnt)
-    print "x,y,w,h", x,y,w,h
+    print " Bounding Rectangle: ({0},{1}), ({2},{3})".format(x,y,(x+w),(y+h))
     roi=img[y:y+h,x:x+w]
     # cv2.imwrite(str(idx) + '.jpg', roi)
     # To draw a rectangle, you need T-L corner and B-R corner
-    cv2.rectangle(im,(x,y),(x+w,y+h),(200,0,0),2)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(200,0,0),2)
+
     # Image moments help you to calculate center of mass, area of object, etc.
     # cv2.moments() gives dictionary of all moment values calculated
-    # area = cv2.contourArea(cnt)
-    # x,y,w,h = cv2.boundingRect(cnt)
-    # rect_area = w*h
-    # extent = float(area)/rect_area
     M = cv2.moments(cnt)
     # Centroid is given by the relations
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
     centroid = (cx, cy)
-    print 'center of mass', centroid
+    print ' Center of Mass:', centroid
+    # To draw a circle, you need its center coordinates and radius
+    cv2.circle(img, centroid, 3, (255,255,0), -1)
+
     # Contour area is given by the function cv2.contourArea(cnt) or
     area = M['m00']
-    print 'area', area
-    # To draw a circle, you need its center coordinates and radius
-    cv2.circle(im, centroid, 3, (255,255,0), -1)
+    print ' Area:', area
+    # area = cv2.contourArea(cnt)
+    # x,y,w,h = cv2.boundingRect(cnt)
+    rect_area = w*h
+    extent = float(area)/rect_area
+    print ' Extent:', round(extent, 3)
 
-print "showing image"
-cv2.imshow('Columbia Campus Map',im)
-cv2.waitKey(0)
 
+cv2.namedWindow('Columbia Campus Map')
+cv2.setMouseCallback('Columbia Campus Map',draw_circle)
+print "Showing image..."
+
+# cv2.waitKey(0)
 
 while(1):
-    cv2.imshow('image',img)
+    cv2.imshow('Columbia Campus Map',img)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         print 'pressed m'
