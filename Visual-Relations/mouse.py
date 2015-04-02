f47aa1fc49bf52a9e46273bc3c1a6696d465d168
Nina Baculinao <nb2406@columbia.edu>
2015-04-01 20:35:13 -0400

    feat(mouse.py): detect center of masses and outline bounding boxes

--- Visual-Relations/mouse.py
+++ Visual-Relations/mouse.py
@@ -28,10 +28,34 @@ def draw_circle(event,x,y,flags,param):
             cv2.circle(img,(x,y),5,(0,0,255),-1)
 
 img = cv2.imread('ass3-labeled.pgm', cv2.IMREAD_UNCHANGED)
+im = cv2.imread('ass3-campus.pgm', cv2.IMREAD_UNCHANGED)
 img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+im = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)
 # img = np.zeros((512,512,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
+
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+contours,hierarchy = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+idx = 0
+for cnt in contours:
+    idx += 1
+    x,y,w,h = cv2.boundingRect(cnt)
+    roi=img[y:y+h,x:x+w]
+    # cv2.imwrite(str(idx) + '.jpg', roi)
+    cv2.rectangle(im,(x,y),(x+w,y+h),(200,0,0),2)
+    # find centroid
+    M = cv2.moments(cnt)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    centroid = (cx, cy)
+    cv2.circle(im, centroid, 3, (255,255,0), -1)
+
+print "showing image"
+cv2.imshow('img',im)
+cv2.waitKey(0)
+
 
 while(1):
     cv2.imshow('image',img)
