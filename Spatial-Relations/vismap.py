91df764dde73b2eac9df5ce382cfed6a0f0bc261
Nina Baculinao <nb2406@columbia.edu>
2015-04-08 00:16:56 -0400

    feat(vismap.py): load names from file and building numbers

--- Spatial-Relations/vismap.py
+++ Spatial-Relations/vismap.py
@@ -5,15 +5,40 @@ import numpy as np
 # Globals
 # ============================================================
 
+# set such that full image array is printed out
+np.set_printoptions(threshold=np.nan)
+
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
 ix,iy = -1,-1
-im = cv2.imread('ass3-labeled.pgm', cv2.IMREAD_UNCHANGED)
-img = cv2.imread('ass3-campus.pgm', cv2.IMREAD_UNCHANGED)
-img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-im = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)
-imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+map_labeled = cv2.imread('ass3-labeled.pgm', 0) # load map_labeled as grayscale
+map_campus = cv2.imread('ass3-campus.pgm', 1) # load map_campus as color
 
+imgray = cv2.cvtColor(map_campus,cv2.COLOR_BGR2GRAY) # map_labeled_gray
+
+def print_imarray(im):
+    w = len(im[0])
+    for i in xrange(len(im)):
+        print im[i][0], im[i][1], im[i][3]
+
+def grayscale(image):
+    """Convert image into black and white with (R+G+B)/3"""
+    h = len(image)
+    w = len(image[0])
+    gray = np.zeros(shape=(h,w))
+    for row in xrange(h):
+        for col in xrange(w):
+            # compose gray image pixel by pixel
+            pixel = image[row][col]
+            rgb = int(pixel[0]) + int(pixel[1]) + int(pixel[2])
+            g = int(round((rgb / 3.0),0)) # round up
+            gray[row][col] = g
+    # gray_img = cv2.imread(gray, cv2.IMREAD_UNCHANGED)
+    cv2.imwrite('./gray.png', gray)
+    cv2.imwrite('./unchanged.png', image)
+    return gray
+
+# print gray
 
 # ============================================================
 # User Interface
@@ -32,41 +57,44 @@ def draw_circle(event,x,y,flags,param):
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
             if mode == True:
-                # cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-                cv2.circle(img,(x,y),5,(0,255,0),-1)
+                # cv2.rectangle(map_campus,(ix,iy),(x,y),(0,255,0),-1)
+                cv2.circle(map_campus,(x,y),5,(0,255,0),-1)
             else:
-                cv2.circle(img,(x,y),5,(0,0,255),-1)
+                cv2.circle(map_campus,(x,y),5,(0,0,255),-1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if mode == True:
-            # cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            cv2.circle(img,(x,y),5,(0,255,0),-1)
+            # cv2.rectangle(map_campus,(ix,iy),(x,y),(0,255,0),-1)
+            cv2.circle(map_campus,(x,y),5,(0,255,0),-1)
         else:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
+            cv2.circle(map_campus,(x,y),5,(0,0,255),-1)
 
 # ============================================================
 # The "What"
 # ============================================================
 
 def which_building(x,y):
-    idx = int(im[y][x][2])
+    idx = int(map_labeled[y][x][2])
     return idx
 
 def load_names(filename):
-    names = []
+    names = {}
     infile = open(filename, 'rU')
-    for idx in xrange(27):
-        name = infile.readline().replace('"', '').split('=')[1].rstrip('\r\n')
-        names.append(name)
-    print names
+    while True:
+        try:
+            line = infile.readline().replace('"', '').split('=')
+            n = line[0]
+            name = line[1].rstrip('\r\n')
+            names[n] = name
+        except IndexError:
+            break
     return names
 
-def analyze_buildings():
+def analyze_buildings(names):
     buildings = {}
-    names = load_names('ass3-table.txt')
+    num_buildings = len(names)
     contours,hierarchy = cv2.findContours(imgray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    idx = 27
     for cnt in contours:
         # To get all the points which comprise an object
         # Numpy function gives coordinates in (row, col)
@@ -78,21 +106,22 @@ def analyze_buildings():
         #pixelpoints = cv2.findNonZero(mask)
 
         # Use color to determine index and identity
-        color = cv2.mean(im,mask = mask)
-        if (color[2] < 0.9):
+        color = cv2.mean(map_labeled,mask = mask)
+        print color
+        if (color[0] < 0.9):
             print 'ignored the cs courtyard'
             continue
-        idx = int(color[2])
-        print 'Building', idx, ':', names[idx-1]
+        idx = str(int(round(color[0], 0)))
+        print 'Building', idx, ':', names[idx]
 
         # Let (x,y) be top-left coordinate and (w,h) be width and height
         # Find min, max value of x, min, max value of y
         x,y,w,h = cv2.boundingRect(cnt)
-        print " Bounding Rectangle: ({0},{1}), ({2},{3})".format(x,y,(x+w),(y+h))
-        roi = img[y:y+h,x:x+w]
+        print " Minimum Bounding Rectangle: ({0},{1}), ({2},{3})".format(x,y,(x+w),(y+h))
+        roi = map_campus[y:y+h,x:x+w]
         # cv2.imwrite(str(idx) + '.jpg', roi)
         # To draw a rectangle, you need T-L corner and B-R corner
-        cv2.rectangle(img,(x,y),(x+w,y+h),(200,0,0),2)
+        cv2.rectangle(map_campus,(x,y),(x+w,y+h),(200,0,0),2)
 
         # Image moments help you to calculate center of mass, area of object, etc.
         # cv2.moments() gives dictionary of all moment values calculated
@@ -103,7 +132,7 @@ def analyze_buildings():
         centroid = (cx, cy)
         print ' Center of Mass:', centroid
         # To draw a circle, you need its center coordinates and radius
-        cv2.circle(img, centroid, 3, (255,255,0), -1)
+        # cv2.circle(map_campus, centroid, 3, (255,255,0), -1)
 
         # Contour area is given by the function cv2.contourArea(cnt) or
         area = M['m00']
@@ -114,6 +143,8 @@ def analyze_buildings():
         extent = float(area)/rect_area
         print ' Extent:', round(extent, 3)
 
+        # label = str(idx) + ' : ' + str(area) + ' : ' + str(extent)
+        # cv2.putText(map_campus, str(idx), (cx,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.3, 255)
 
         # check curve for convexity defects and correct it
         # pass in contour points, hull, !returnPoints return indices
@@ -126,12 +157,18 @@ def analyze_buildings():
                 end = tuple(cnt[e][0])
                 far = tuple(cnt[f][0])
                 # print start, end, far
-                cv2.line(img,start,end,[0,255,0],1)
-                cv2.circle(img,far,3,[255,0,255],-1)
+                cv2.line(map_campus,start,end,[0,255,0],1)
+                cv2.circle(map_campus,far,3,[255,0,255],-1)
 
         # this just draws the rect again
-        #cv2.drawContours(img, contours, 0, (0,0,255), 1)
-    return buildings
+        #cv2.drawContours(map_campus, contours, 0, (0,0,255), 1)
+
+    # find corners - this method is buggy
+    # dst = cv2.cornerHarris(imgray,3,3,0.2)
+    # dst = cv2.dilate(dst,None)
+    # map_campus[dst>0.01*dst.max()]=[0,0,255]
+
+    return buildings, names
 
 # def analyze_shape()
 
@@ -141,19 +178,20 @@ def analyze_buildings():
 
 def main():
 
-    # img = np.zeros((512,512,3), np.uint8)
+    # map_campus = np.zeros((512,512,3), np.uint8)
 
     # Analyze image
-    buildings = analyze_buildings()
+    names = load_names('ass3-table.txt')
+    buildings = analyze_buildings(names)
 
     cv2.namedWindow('Columbia Campus Map')
-    cv2.setMouseCallback('Columbia Campus Map',draw_circle)
+    cv2.setMouseCallback('Columbia Campus Map', draw_circle)
     print "Showing image..."
 
     # cv2.waitKey(0)
 
     while(1):
-        cv2.imshow('Columbia Campus Map',img)
+        cv2.imshow('Columbia Campus Map', map_campus)
         k = cv2.waitKey(1) & 0xFF
         if k == ord('m'):
             print 'pressed m'
