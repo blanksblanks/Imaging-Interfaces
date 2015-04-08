9e0e62c784ee7a89e5274d5585b1ff9ef3f48fee
Nina Baculinao <nb2406@columbia.edu>
2015-04-08 16:31:01 -0400

    feat(vismap.py): finally able to check midpoint/corner values (note: image uses (x,y), numpy array uses (y,x))

--- Spatial-Relations/vismap.py
+++ Spatial-Relations/vismap.py
@@ -111,7 +111,7 @@ def id_building(cnt):
     else:
         return None
 
-def measure_building(cnt):
+def measure_building(cnt, print_rect=False):
     # Let (x,y) be top-left coordinate and (w,h) be width and height
     # Find min, max value of x, min, max value of y
     x,y,w,h = cv2.boundingRect(cnt)
@@ -120,7 +120,8 @@ def measure_building(cnt):
     roi = map_campus[y:y+h,x:x+w]
     # cv2.imwrite(str(idx) + '.jpg', roi)
     # To draw a rectangle, you need T-L corner and B-R corner
-    cv2.rectangle(map_campus,(x,y),(x+w,y+h),(200,0,0),2)
+    if print_rect:
+        cv2.rectangle(map_campus,(x,y),(x+w,y+h),(200,0,0),2)
 
     # Image moments help you to calculate center of mass, area of object, etc.
     # cv2.moments() gives dictionary of all moment values calculated
@@ -162,7 +163,7 @@ def measure_building(cnt):
     #cv2.drawContours(map_campus, contours, 0, (0,0,255), 1)
 
     # find corners - this method is buggy
-    # dst = cv2.cornerHarris(imgray,3,3,0.2)
+    # dst = cv2.cornerHarris(map_binary,3,3,0.2)
     # dst = cv2.dilate(dst,None)
     # map_campus[dst>0.01*dst.max()]=[0,0,255]
 
@@ -189,12 +190,157 @@ def analyze_buildings(names):
         building['centroid'] = centroid
         building['area'] = area
         building['extent'] = extent
+        building['cnt'] = cnt
         buildings[(idx-1)] = building
 
-    analyze_areas(buildings)
-    analyze_extents(buildings)
+    max_area, min_area = analyze_areas(buildings) # add True arg to print results
+    for building in buildings:
+        size = describe_size(building, max_area)
+        describe_shape(building)
+        # shape = describe_shape(building)
+        # multiple = describe_multiplicity
+    # analyze_extents(buildings)
+    # analyze_shapes(buildings)
 
     return buildings
+
+def analyze_extents(buildings):
+    """Sort buildings by extent and determine cutoff for rectangles"""
+    print 'Analyzing building extents (area/mbr) and convexity...'
+    num_buildings = len(buildings)
+    sorted_buildings = sorted(buildings, key=lambda k:-k['extent'])
+    indices = [(sorted_buildings[i]['number']-1) for i in range(num_buildings)]
+    for i in indices:
+        building = buildings[i]
+        convex = cv2.isContourConvex(building['cnt'])
+        print round(building['extent'],4), '\t', convex, '\t', i+1, building['name']
+
+def analyze_areas(buildings, print_results=False):
+    """Sort buildings by area, determine cutoff for size and return max"""
+    num_buildings = len(buildings)
+    sorted_buildings = sorted(buildings, key=lambda k:-k['area'])
+    indices = [(sorted_buildings[i]['number']-1) for i in range(num_buildings)]
+    areas = [(sorted_buildings[i]['area']) for i in range(num_buildings)]
+
+    max_area = areas[0]
+    avg_area = sum(areas)/num_buildings
+    min_area = areas[-1]
+
+    # Print results to analyze cutoffs for size categories
+    if (print_results):
+        print 'Analyzing building areas...'
+        ratios = [round(areas[i]/max_area,3) for i in range(num_buildings)]
+        ratio_diffs = [round((ratios[i+1]-ratios[i]),3) for i in range(num_buildings-1)]
+        ratio_diffs.insert(0,0)
+        max_area_ratios = [round(max_area/areas[i],3) for i in range(num_buildings)]
+
+        print 'Max Area:', max_area, '\nAverage:', round(avg_area, 3)
+        print 'Area\tRatio r\tDiff r\tMax r\tBuilding'
+        for i in xrange(num_buildings):
+            idx = indices[i]
+            print areas[i], '\t', ratios[i], '\t', ratio_diffs[i], '\t', max_area_ratios[i], '\t', idx+1, buildings[idx]['name']
+
+    return max_area, min_area
+
+def analyze_shapes(buildings):
+    """Sort building by shape similarity (not very good results)"""
+    print 'Analyzing shape similarity with cv2.matchShapes...'
+    num_buildings = len(buildings)
+    shape_sim = {}
+    for i in xrange(num_buildings):
+        for j in xrange(i+1, num_buildings):
+            cnt1 = buildings[i]['cnt']
+            cnt2 = buildings[j]['cnt']
+            ret = cv2.matchShapes(cnt1,cnt2, 1,0.0)
+            shape_sim[(i,j)] = ret
+    sorted_sim = sorted([(value,key) for (key,value) in shape_sim.items()])
+    for sim in sorted_sim[:40]:
+        bldg1 = sim[1][0]
+        bldg2 = sim[1][1]
+        print round(sim[0],4), '\t', buildings[bldg1]['name'], '&', buildings[bldg2]['name']
+
+def describe_size(building, max_area):
+    ratio = building['area']/max_area
+    if ratio > 0.7: # cutoff at College Walk
+        return 'colossal'
+    if ratio < 0.4: # cutoff at Journalism & Furnald
+        return 'large'
+    elif ratio > 0.15: # cutoff at Philosophy
+        return 'middling'
+    elif ratio > 0.1: # cutoff Earl Hall
+        return 'small'
+    else:
+        return 'miniscule'
+
+def describe_shape(building):
+    tolerance = 2
+    # map_l = cv2.imread('ass3-labeled.pgm', 0)
+    # print map_l
+    # Recall mbr = [(x,y),(x+w,y+h)]
+    # nw = building['mbr'][0]
+    # se = building['mbr'][1]
+    # ne = (se[0],nw[1]) # (x+w,y)
+    # sw = (nw[0],se[1]) # (x,y+h)
+    # # tl = (x+w,y)
+    # # br = (x,y+h)
+
+    # # Extract x,y,w,h coordinates
+    # x = [0] + tolerance
+    # y = nw[1] + tolerance
+    # w = se[0] - x - tolerance
+    # h = se[1] - y - tolerance
+
+    x,y,w,h = cv2.boundingRect(building['cnt'])
+    x += tolerance
+    y += tolerance
+    w -= 2*tolerance
+    h -= 2*tolerance
+
+    # Extract four corners
+    nw = (x,y)
+    se = (x+w,y+h)
+    ne = (x+w,y)
+    sw = (x,y+h)
+
+    # Extract midpoints on every wall face
+    n = (x+(w/2),y)
+    e = (x+w,y+(h/2))
+    s = (x+(w/2),y+h)
+    w = (x,y+(h/2))
+
+    corners = [nw,se,ne,sw]
+    midpoints = [n,e,s,w]
+    corners_filled = [] # nw, ne, se, sw
+    midpoints_filled = [] # n, e, s, w
+
+    print building['name']
+
+    try :
+        for corner in corners:
+            # print 'Map labeled corner: ', map_labeled[corner]
+            if map_labeled[tuple(reversed(corner))] == building['number']:
+                corners_filled.append(1)
+                cv2.circle(map_campus, corner, 1, (255,255,0), -1)
+            else:
+                corners_filled.append(0)
+                cv2.circle(map_campus, corner, 1, (0,0,255), -1)
+
+        for midpoint in midpoints:
+            # 'Map labeled midpoint: ', map_labeled[midpoint]
+            if map_labeled[tuple(reversed(midpoint))] == building['number']:
+                midpoints_filled.append(1)
+                cv2.circle(map_campus, midpoint, 1, (255,255,0), -1)
+            else:
+                midpoints_filled.append(0)
+                cv2.circle(map_campus, midpoint, 1, (0,0,255), -1)
+    except (IndexError):
+        print IndexError
+
+    print 'corners', corners_filled
+    print 'midpoints', midpoints_filled
+
+
+
 
 def print_info(buildings):
     for building in buildings:
@@ -203,55 +349,6 @@ def print_info(buildings):
         print ' Center of Mass:', building['centroid']
         print ' Area:', building['area']
         # print ' Description', building['description']
-
-def analyze_extents(buildings):
-    """Sort buildings by extent and determine cutoff for rectangles"""
-    print 'Analyzing building extents (area/mbr)...'
-    num_buildings = len(buildings)
-    sorted_buildings = sorted(buildings, key=lambda k:-k['extent'])
-    indices = [(sorted_buildings[i]['number']-1) for i in range(num_buildings)]
-    for i in indices:
-        building = buildings[i]
-        print round(building['extent'],4), '\t', i+1, building['name']
-
-def analyze_areas(buildings):
-    """Sort buildings by area and determine cutoff for rize"""
-    print 'Analyzing building areas...'
-    num_buildings = len(buildings)
-    sorted_buildings = sorted(buildings, key=lambda k:-k['area'])
-    indices = [(sorted_buildings[i]['number']-1) for i in range(num_buildings)]
-    areas = [(sorted_buildings[i]['area']) for i in range(num_buildings)]
-
-    biggest = areas[0]
-    average = sum(areas)/num_buildings
-    ratios = [biggest/areas[i] for i in range(num_buildings)]
-    # diffs = [(areas[i+1]-areas[i])/(areas[i+1]+areas[i]) for i in range(n-1)]
-    # diffs.insert(0,0)
-
-    print 'Biggest:', biggest
-    print 'Average:', round(average, 3)
-
-    for i in xrange(num_buildings):
-        idx = indices[i]
-        print areas[i], '\t', round(ratios[i],3), '\t', idx+1, buildings[idx]['name']
-
-def describe_size(buildings):
-    for building in buildings:
-        if building['area'] < 1:
-            return 'cool'
-            # sizes = []
-    # for ratio in ratios:
-    #     if ratio < 0.146: # cutoff Lion's Court
-    #         sizes.append('dimunitive')
-    #     elif ratio < 0.416: # cutoff Journalism & Furnald
-    #         sizes.append('middling')
-    #     elif ratio < 0.874:
-    #         sizes.append('large')
-    #     else:
-    #         sizes.append('colossal')
-
-
-
 
 
 # ============================================================
